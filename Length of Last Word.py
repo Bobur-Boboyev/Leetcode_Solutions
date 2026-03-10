ddf5a8d1a53fc389891a072cbbf3b27ee550fe26
Bobur-Boboyev <bbyvvv.ai@gmail.com>
2026-03-10 23:36:49 +0500

    .

---
+++ Length of Last Word.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def lengthOfLastWord(self, s):
+        """
+        :type s: str
+        :rtype: int
+        """
+        last = s.split()[-1]
+        return len(last)
